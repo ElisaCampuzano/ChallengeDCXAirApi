b0a1be91f4fe3903a0ed416f0fb3066c19d72cd3
Elisabeth Campuzano <ecampuzano@sigmaingenieria.net>
2024-11-30 16:22:23 -0500

    Agregar archivos de proyecto.

---
+++ ChallengeDCXAirApi/Controllers/DCXAirController.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeDCXAirApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DCXAirController : ControllerBase
+    {
+        
+    }
+}
---
+++ Domain/Entities/Flight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Flight
+    {
+        public string Id { get; set; }
+        public Transport Transport { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public double Price { get; set; }
+    }
+}
